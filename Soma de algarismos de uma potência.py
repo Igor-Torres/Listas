eb61c63071590267db57cdf9f35ed0433f627731
Igor-Torres <48731661+Igor-Torres@users.noreply.github.com>
2019-03-28 16:26:48 -0300

    Add files via upload

--- Soma de algarismos de uma potência.py
+++ Soma de algarismos de uma potência.py
@@ -8,12 +8,14 @@ def fun():
     p=[]
     print('Soma de todos os algarismos de uma potência (sem raíz):\n')
     q=int(input('defina a base da potencia: '))
-    w=int(input('escolha o expoente da potencia:\n '))
+    w=int(input('escolha o expoente da potencia: '))
     e=q**w
     u=tuple(str(e))
     for i in range(len(u)):
         z+=int(u[i])
-    print('O resultado da sua potência é '+str(e)+', e a soma de seus algarismos é:\n ')
+    print('')
+    print('O resultado da sua potência é '+str(e)+', e a soma de seus algarismos é: ')
+    print('')
     p.append(z)
     print(p)
 
