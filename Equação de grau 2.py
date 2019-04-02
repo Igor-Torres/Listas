fc63dd185456794345bd154d7de18c101044081b
Igor-Torres <48731661+Igor-Torres@users.noreply.github.com>
2019-04-02 14:54:42 -0300

    Update Equação de grau 2.py

--- Equação de grau 2.py
+++ Equação de grau 2.py
@@ -3,49 +3,57 @@
    DRE: 119034669
    Curso: Matemática Aplicada
    Matéria: Tópicos de Mat. Apl. A'''
-loop=0
-while loop<10:
     
-    print ('-------Equação do segundo grau-------')
-    print ('')
-    print('No formato ax²+bx+c (apenas raízes reais)')
-    print ('')
-    a=float(input("Defina o coeficiente 'a', que antecede x²:"))
-    b=float(input("Defina o coeficiente 'b', que antecede x:"))
-    c=float(input("Defina o coeficiente 'c', que é independente:"))
-    if(a!=0):
-        delta=(float(b**2 - 4*a*c))
-    if(delta>0):
-            print ('')
-            print("Essas são as raízes reais da equação ("+str(a)+")x²+("+str(b)+")x+("+str(c)+"):")
-            print(float(-b + (delta)**0.5)/(2*a))
-            print(float(-b - (delta)**0.5)/(2*a))
-            print('')
-            print('')
-            a=float(input('Deseja calcular outra equação?\n1)Sim.\n2)Não.\n\n'))#menu de opções
-            print('')
-            if a==2:
-                print('\nPrograma encerrado.')
-                break
-    if(delta==0):
-            print ('')
-            print("Essa é a raiz única da equação ("+str(a)+")x²+("+str(b)+")x+("+str(c)+"):")
-            print(float(-b + (delta)**0.5)/(2*a))
-            print('')
-            print('')
-            a=float(input('Deseja calcular outra equação?\n1)Sim.\n2)Não.\n\n'))
-            print('')
-            if a==2:
-                print('\nPrograma encerrado.')
-                break
-    if(delta<0):
-            print ('')
-            print("A equação ("+str(a)+")x²+("+str(b)+")x+("+str(c)+") não possui raízes reais.")
-            print('')
-            print('')
-            a=float(input('Deseja calcular outra equação?\n1)Sim.\n2)Não.\n\n'))
-            print('')
-            if a==2:
-                print('\nPrograma encerrado.')
-                break
+  
+  def eq2():#nome da função
 
+    
+  print ('-------Equação do segundo grau-------')
+  print ('')
+  print('No formato ax²+bx+c (apenas raízes reais)')
+  print ('')
+  a=float(input("Defina o coeficiente 'a', que antecede x²:"))
+  b=float(input("Defina o coeficiente 'b', que antecede x:"))
+  c=float(input("Defina o coeficiente 'c', que é independente:"))
+  if(a!=0):
+      delta=(float(b**2 - 4*a*c))
+  if(delta>0):
+          print ('')
+          print("Essas são as raízes reais da equação ("+str(a)+")x²+("+str(b)+")x+("+str(c)+"):")
+          print(float(-b + (delta)**0.5)/(2*a))
+          print(float(-b - (delta)**0.5)/(2*a))
+          print('')
+          print('')
+          a=float(input('Deseja calcular outra equação?\n1)Sim.\n2)Não.\n\n'))#menu de opções
+          print('')
+          if a==2:
+              print('Programa encerrado.')
+              return
+          if a==1:
+              eq2()    
+  if(delta==0):
+          print ('')
+          print("Essa é a raiz única da equação ("+str(a)+")x²+("+str(b)+")x+("+str(c)+"):")
+          print(float(-b + (delta)**0.5)/(2*a))
+          print('')
+          print('')
+          a=float(input('Deseja calcular outra equação?\n1)Sim.\n2)Não.\n\n'))
+          print('')
+          if a==2:
+              print('Programa encerrado.')
+              return
+          if a==1:
+              eq2()    
+  if(delta<0):
+          print ('')
+          print("A equação ("+str(a)+")x²+("+str(b)+")x+("+str(c)+") não possui raízes reais.")
+          print('')
+          print('')
+          a=float(input('Deseja calcular outra equação?\n1)Sim.\n2)Não.\n\n'))
+          print('')
+          if a==2:
+              print('Programa encerrado.')
+              return
+          if a==1:
+              eq2()
+              
