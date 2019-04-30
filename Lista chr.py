8a707eaa344b6835fa82cdbe9414c622507a99fd
Igor-Torres <48731661+Igor-Torres@users.noreply.github.com>
2019-04-30 15:15:14 -0300

    Add files via upload

--- Lista chr.py
+++ Lista chr.py
@@ -1,18 +1,12 @@
-def tabela (x,y,z):
+def tabela (x=32,y=128,z=5):
     print("+----------|-----|-----|-----+\n| binário  | oct | hex | chr |\n+----------|-----|-----|-----+")
     for j in range (x,y):
-        print("| {0:8b} | {0:3o} | {0:3X} |  {0:1c}  |\n+----------|-----|-----|-----+".format(j))
+        q='{0:08b}'.format(j)
+        a=q[:4]
+        b=q[4:]
+        print("|{1} {2} | {0:3o} | {0:3X} |  {0:1c}  |\n+----------|-----|-----|-----+".format(j,a,b))
         if (j-x)%z==0 and j-x!=0 and j!=y-1:
             print('\n+----------|-----|-----|-----+\n| binário  | oct | hex | chr |\n+----------|-----|-----|-----+')
         elif j==y-1:
            pass
- '''u='{0:8b}'.format(j)
-    p=tuple(u)
-    if len(p)<8:
-        i=-1
-        for t in len(p):
-           i+=1
-           k='0'+str(p[i])
-           print(k) 
-'''
- #séra q da certo isso de cima pra adicionar um zero ao binário?
+ 
